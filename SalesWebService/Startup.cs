0503bd70e664a800a9a4cfa4768b8322118398f8
Pamella Dionisio <pamella.dioniso@martinellilabs.com.br>
2023-05-27 23:37:42 -0300

    Aplicando Migration

--- SalesWebService/Startup.cs
+++ SalesWebService/Startup.cs
@@ -37,7 +37,8 @@ namespace SalesWebService
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SalesWebServiceContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebServiceContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SalesWebServiceContext"), builder =>
+                         builder.MigrationsAssembly("SalesWebService")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
