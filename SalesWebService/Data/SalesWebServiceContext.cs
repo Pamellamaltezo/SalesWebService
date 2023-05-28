3c64fe137023ee5d9d2f5713587206d38f91d270
Pamella Dionisio <pamella.dioniso@martinellilabs.com.br>
2023-05-27 23:11:35 -0300

    CRUD Scaffolding

---
+++ SalesWebService/Data/SalesWebServiceContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebService.Models;
+
+namespace SalesWebService.Data
+{
+    public class SalesWebServiceContext : DbContext
+    {
+        public SalesWebServiceContext (DbContextOptions<SalesWebServiceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebService.Models.Department> Department { get; set; }
+    }
+}
--- SalesWebService/Startup.cs
+++ SalesWebService/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebService.Data;
 
 namespace SalesWebService
 {
@@ -33,6 +35,9 @@ namespace SalesWebService
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SalesWebServiceContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SalesWebServiceContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
